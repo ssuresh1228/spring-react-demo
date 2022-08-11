c7109bf2f0f1d5ab061fd5a5c9cbdfa3290c3661
Sai Suresh <saisiddharth1@gmail.com>
2022-08-11 10:44:35 -0700

    fixed submodule

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import AddEmployee from './components/AddEmployee';
+import Navbar from './components/Navbar';
+
+
+function App() {
+  return(
+    <>
+    <Navbar />
+    <AddEmployee />
+    </>
+  ) 
+    
+}
+
+export default App;
+
+//28:40 
