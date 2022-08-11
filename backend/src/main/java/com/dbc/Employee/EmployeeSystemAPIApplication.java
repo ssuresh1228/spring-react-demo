f54fa06336017df3b378972ea91ae0765b6d14a3
Sai Suresh <saisiddharth1@gmail.com>
2022-08-11 10:36:32 -0700

    init

---
+++ backend/src/main/java/com/dbc/Employee/EmployeeSystemAPIApplication.java
@@ -0,0 +1,13 @@
+package com.dbc.Employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeSystemAPIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeSystemAPIApplication.class, args);
+	}
+
+}
